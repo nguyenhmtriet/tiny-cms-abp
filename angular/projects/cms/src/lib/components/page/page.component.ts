a79fc3a101e24aadc69540776bba29fa67ef9376
Triet Nguyen <>
2022-06-26 11:29:36 +0700

    set page title

--- angular/projects/cms/src/lib/components/page/page.component.ts
+++ angular/projects/cms/src/lib/components/page/page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Params } from '@angular/router';
 import { CmsManagementService } from 'cms/config';
 import { PageContentDto } from 'projects/cms/config/src/models';
@@ -12,20 +13,27 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class PageComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<boolean>();
+  private title: string;
 
   pageContent: PageContentDto;
 
   constructor(
     private cmsManagementService: CmsManagementService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
   ) {
+    this.title = this.titleService.getTitle();
     this.activatedRoute.params.pipe(takeUntil(this.destroy$)).subscribe((params: Params) => {
-      this.cmsManagementService.get(params.id).subscribe(res => (this.pageContent = res));
+      this.cmsManagementService.get(params.id).subscribe(res => {
+        this.pageContent = res;
+        this.titleService.setTitle(res.title);
+      });
     });
   }
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.complete();
+    this.titleService.setTitle(this.title);
   }
 
   ngOnInit(): void {}
